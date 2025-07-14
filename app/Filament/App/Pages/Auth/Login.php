b8e6f4897874fbfac804b85bbe88f662f7ea36b1
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-07-14 13:58:50 -0300

    refactor(auth): centralize login functionality into shared base class
    Moved shared `getCredentialsFromFormData` logic to a new abstract `Login` class under `App\Filament\Auth\Pages`. Updated `Admin` and `App` login pages to extend the new base class, improving maintainability and consistency.

--- app/Filament/App/Pages/Auth/Login.php
+++ app/Filament/App/Pages/Auth/Login.php
@@ -2,14 +2,6 @@
 
 namespace App\Filament\App\Pages\Auth;
 
-class Login extends \Filament\Auth\Pages\Login
+class Login extends \App\Filament\Auth\Pages\Login
 {
-    protected function getCredentialsFromFormData(array $data): array
-    {
-        return [
-            'email' => $data['email'],
-            'password' => $data['password'],
-            'status' => true,
-        ];
-    }
 }
