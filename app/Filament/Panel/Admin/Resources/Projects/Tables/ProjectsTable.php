cd579abd227f1973deafb03b28c735038e37146d
jeffersongoncalves <411493+jeffersongoncalves@users.noreply.github.com>
2025-07-15 01:27:30 +0000

    Fix styling

--- app/Filament/Panel/Admin/Resources/Projects/Tables/ProjectsTable.php
+++ app/Filament/Panel/Admin/Resources/Projects/Tables/ProjectsTable.php
@@ -2,8 +2,6 @@
 
 namespace App\Filament\Panel\Admin\Resources\Projects\Tables;
 
-use Filament\Actions\BulkActionGroup;
-use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
